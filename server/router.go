50e0588d3a20887ec4f67babe25b3313b69022a2
yunee <yhjang@symverse.com>
2020-02-21 15:44:08 +0900

    remove hard coding

--- server/router.go
+++ server/router.go
@@ -14,60 +14,29 @@ func SetupRouter(router *gin.Engine) *gin.Engine {
 	router.LoadHTMLGlob("templates/*.html")
 	router = setRouterDefault(router)
 
-	v1 := router.Group("v1")
-	setRouterV1(v1)
+	//wild card :page를 사용하면서 /v1도 그곳에 포함.. 라우터 그룹하려니 아래와같은 문제가 생김..
+	//panic: 'v1' in new path '/v1/login' conflicts with existing wildcard ':page' in exist
 
-	v2 := router.Group("/v2")
-	setRouterV2(v2)
+	//setting router group
+	// v1 := router.Group("/v1")
+	// setRouterV1(v1)
+
+	// v2 := router.Group("/v2")
+	// setRouterV2(v2)
 	return router
 }
 
 func setRouterDefault(router *gin.Engine) *gin.Engine {
 
 	// Using GET, POST, PUT, PATCH, DELETE and OPTIONS
-	fmt.Println("setRouterDefault")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil) //세번째인자 무엇인지 모르겠음
 	})
-	router.GET("/charts", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "charts.html", nil) //세번째인자 무엇인지 모르겠음
+	router.GET("/:page", func(c *gin.Context) {
+		page := c.Param("page")
+		c.HTML(http.StatusOK, page, nil) //세번째인자 무엇인지 모르겠음
 	})
-	router.GET("/tables", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "tables.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/utilities-color", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "utilities-color.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/utilities-other", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "utilities-other.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/blank", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "blank.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/buttons", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "buttons.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/cards", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "cards.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/forgot-password", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "forgot-password.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/register", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "register.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/utilities-animation", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "utilities-animation", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/404", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "404.html", nil) //세번째인자 무엇인지 모르겠음
-	})
-	router.GET("/utilities-border", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "utilities-border.html", nil) //세번째인자 무엇인지 모르겠음
-	})
+
 	return router
 }
 
@@ -93,6 +62,8 @@ func callV1(c *gin.Context) {
 }
 
 func callV2(c *gin.Context) {
+	fmt.Println("CALL ! V2 !!")
+
 	name := c.Param("name")
 	c.String(http.StatusOK, "Hello V2 %s", name)
 }
